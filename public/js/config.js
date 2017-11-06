33717842a81595412fb09c2a9c3f1dea3945b91a
botue <botue@sina.com>
2017-11-06 15:20:03 +0800

    完成了商品管理

--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,21 @@
 require.config({
     baseUrl: '/public',
     paths: {
-        jquery: 'assets/jquery/jquery.min'
+        jquery: 'assets/jquery/jquery.min',
+        template: 'assets/artTemplate/template-web',
+        uploadify: 'assets/uploadify/jquery.uploadify.min'
+    },
+    // 如果某个第三方的类库不支持 AMD，通过 shim 
+    // 可以实现类似模块的用法
+    shim: {
+        // 模块有何特点？
+        uploadify: {
+            // 1、通过 exports 可以将非模块的方法或属性
+            // 公开出来（相当于标准模块中 return 的作用）
+            // exports: 
+            
+            // 2、通过 deps 可以依赖其它模块
+            deps: ['jquery']
+        }
     }
 });
